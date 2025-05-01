f08362caad728898c80fbb44c86ce0c636bb45f1
saarock <saarock4646@gmail.com>
2025-05-01 12:28:07 +0545

    project initialize

---
+++ android/app/src/main/kotlin/com/example/chime/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.chime
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,12 @@
+// All the import statement starts
+
+import 'package:chime/app.dart';
+import 'package:flutter/material.dart';
+
+// All the important statement ends
+
+//Main entry point of the Chime App
+
+void main() {
+  runApp(App());
+}
